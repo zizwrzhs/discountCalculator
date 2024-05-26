6553b13673fa6869b626d9d5e03a4a1339431831
Giorgos Georgopetris <georgopetrisg@gmail.com>
2024-05-26 20:49:51 +0300

    Almost done

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,20 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Title = new System.Windows.Forms.Label();
+            this.origPrice = new System.Windows.Forms.TextBox();
+            this.discount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.Title.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Title.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Title.Location = new System.Drawing.Point(0, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(400, 60);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "Discount Calculator";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // origPrice
+            // 
+            this.origPrice.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.origPrice.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.origPrice.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.origPrice.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.origPrice.Location = new System.Drawing.Point(140, 93);
+            this.origPrice.Name = "origPrice";
+            this.origPrice.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.origPrice.Size = new System.Drawing.Size(120, 29);
+            this.origPrice.TabIndex = 2;
+            this.origPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.origPrice.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.origPrice.Enter += new System.EventHandler(this.origPrice_Enter);
+            this.origPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.origPrice_KeyPress);
+            this.origPrice.Leave += new System.EventHandler(this.origPrice_Leave);
+            // 
+            // discount
+            // 
+            this.discount.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.discount.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.discount.Font = new System.Drawing.Font("Calibri", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discount.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.discount.Location = new System.Drawing.Point(140, 160);
+            this.discount.Name = "discount";
+            this.discount.Size = new System.Drawing.Size(120, 29);
+            this.discount.TabIndex = 2;
+            this.discount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.discount.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.discount.Enter += new System.EventHandler(this.discount_Enter);
+            this.discount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.discount_KeyPress);
+            this.discount.Leave += new System.EventHandler(this.discount_Leave);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(145, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Original Price (€):";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(157, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Discount (%):";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(238, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Final Price:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button2.Location = new System.Drawing.Point(216, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(82, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "You Saved:";
+            this.label4.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button1.Location = new System.Drawing.Point(63, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label5.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(216, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 50);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "NaN";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label6.BackColor = System.Drawing.Color.Orange;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(63, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 50);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "NaN";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 501);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(400, 500);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.discount);
+            this.Controls.Add(this.origPrice);
+            this.Font = new System.Drawing.Font("Clab Personal Use", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Discount Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.TextBox origPrice;
+        private System.Windows.Forms.TextBox discount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +18,138 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            origPrice.Text = "e.g. 14.99";
+            origPrice.ForeColor = Color.LightGray;
+
+            discount.Text = "e.g. 20";
+            discount.ForeColor = Color.LightGray;
+
+            label5.Text = "NaN";
+            label6.Text = "NaN";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+        }
+
+        private void discount_Enter(object sender, EventArgs e)
+        {
+            if (discount.Text == "e.g. 20")
+            {
+                discount.Text = "";
+                discount.ForeColor = Color.Black;
+            }
+        }
+
+        private void discount_Leave(object sender, EventArgs e)
+        {
+            if (discount.Text == "")
+            {
+                discount.Text = "e.g. 20";
+                discount.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void origPrice_Enter(object sender, EventArgs e)
+        {
+            if (origPrice.Text == "e.g. 14.99")
+            {
+                origPrice.Text = "";
+                origPrice.ForeColor = Color.Black;
+            }
+        }
+
+        private void origPrice_Leave(object sender, EventArgs e)
+        {
+            if (origPrice.Text == "")
+            {
+                origPrice.Text = "e.g. 14.99";
+                origPrice.ForeColor = Color.LightGray; 
+            }
+        }
+
+        private void origPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.' && (sender as TextBox).Text.Contains('.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void discount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool flag = false;
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.' && (sender as TextBox).Text.Contains('.')) || (e.KeyChar == '-' && (sender as TextBox).Text.Contains('-')))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double num1, num2, result, save;
+
+            if (origPrice.Text == "" || discount.Text == "" || origPrice.Text == "e.g. 14.99" || discount.Text == "e.g. 20")
+            {
+                MessageBox.Show("wrong values");
+            }
+            else
+            {
+
+                num1 = Convert.ToDouble(origPrice.Text);
+                num2 = Convert.ToDouble(discount.Text);
+                result = (double)num1 - (double)num1 * num2 / 100;
+                save = (double)num1 * num2 / 100;
+
+                result = Math.Round(result, 2, MidpointRounding.AwayFromZero);
+                save = Math.Round(save, 2, MidpointRounding.AwayFromZero);
+
+                label5.Text = result.ToString("0.00") + "€";
+
+                label6.Text = save.ToString("0.00") + "€";
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
         }
     }
 }
